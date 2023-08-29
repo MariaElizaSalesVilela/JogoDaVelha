31755fe263ff74421421c3a0e5a2070efc381324
MariaElizaSalesVilela <128425079+MariaElizaSalesVilela@users.noreply.github.com>
2023-08-28 23:36:54 -0300

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -1,22 +1,67 @@
-let ultimaJogada = '0';
+const cellElements = document.querySelector("data-cell")
+const board = document.querySelector("[data-board]")
 
-function marcarJogada(identificador){
-    const quadradoPequeno = document.getElementById(identificador);
+let isCircleTurn 
 
+const winningCobinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    ]2, 4, 6],
+    ]
 
-    const jogada = quadradoPequeno.innerHTML;
-
-    if (jogada != "") {
-        return;
-    }
-
-    if (ultimaJogada == '0'){
-        quadradoPequeno.innerHTML = 'X';
-        ultimaJogada = 'X';
-        return;
-    }
-
-    quadradoPequeno.innerHTML = '0';
-    ultimaJogada = '0';
-
+const startGame = () => {
+    for (const cell of cellElements){
+    cell.addEventListener("click",handleClick, { once: true})
 }
+    isCircleTurn = false
+
+    board.classList.add('x')
+}
+const endGame = (draw) =>  {
+}
+
+const checkForWin = (currentPlayer) => {
+    return winningCombinations.some(combination => {
+        return combination.evety(index => {
+            return cellElements[index].classList.contains(currentPlayer)
+    })
+    })
+}
+
+const placeMark = (cell, classToAdd) =>{
+    cell.classList.add(classToAdd)
+}
+const swapTurns = () => {
+isCircleTurn = !isCircleTurn
+    board.classList.remove('circle')
+    board.classList.remove('x')
+
+    if (isCircleTurn){
+        board.classList.add('circle')
+    }else{
+        board.classList.add("x")
+    
+}
+
+const handleClick = (e) => {
+    
+    const cell = e.target
+    const classToAdd = isCircleTurn ? 'circle' : 'x'
+
+    placeMark(cell, classToAdd)
+
+    const isWin = checkForWin(classToAdd)
+    if (isWin) {
+        console.log("winner")
+
+
+swapTurns()
+    
+}
+
+startGame()
